afa2281fcb1e9a8ef8414169d8a6420084568a92
Lincoln Quick <lincolnmq@gmail.com>
2025-03-20 19:18:40 -0400

    Fix run_parallel extra indent for function def

--- mod6/run_parallel.py
+++ mod6/run_parallel.py
@@ -13,22 +13,22 @@ def process_file(part_num):
     
     print(f"Finished processing {input_file}.")
     
-    def run_parallel(num_parts):
-        start_time = time.perf_counter()
-        
-        processes = []
-        
-        for i in range(num_parts):
-            p = multiprocessing.Process(target=process_file, args=(i + 1,))
-            processes.append(p)
-            p.start()
-        
-        for p in processes:
-            p.join()
-        
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        print(f"Parallel processing ({num_parts} parts) completed in {total_time:.4f} seconds.")
-        
+def run_parallel(num_parts):
+    start_time = time.perf_counter()
+    
+    processes = []
+    
+    for i in range(num_parts):
+        p = multiprocessing.Process(target=process_file, args=(i + 1,))
+        processes.append(p)
+        p.start()
+    
+    for p in processes:
+        p.join()
+    
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    print(f"Parallel processing ({num_parts} parts) completed in {total_time:.4f} seconds.")
+    
 if __name__ == "__main__":
     run_parallel(10)
