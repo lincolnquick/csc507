2f42675e53c43f50e8fe36ffd7a9e2e96b3e320b
Lincoln Quick <lincolnmq@gmail.com>
2025-03-20 19:06:04 -0400

    Create mod6 dir, copied numbers and double_numbers, create split_file, run_parallel, and combine_files

---
+++ mod6/combine_files.py
@@ -0,0 +1,12 @@
+def combine_files(num_parts, final_output):
+    with open(final_output, 'w') as outfile:
+        for i in range(num_parts):
+            part_file = f'newfile_part_{i + 1}.txt'
+            with open(part_file, 'r') as infile:
+                outfile.writelines(infile.readlines())
+    
+    print(f"Combined {num_parts} files into {final_output}.")
+
+if __name__ == "__main__":
+    combine_files(10, 'newfile1_combined.txt')
+    \
---
+++ mod6/split_file.py
@@ -0,0 +1,20 @@
+def split_file(input_file, num_parts):
+    with open(input_file, 'r') as infile:
+        lines = infile.readlines()
+        
+    total_lines = len(lines)
+    chunk_size = total_lines // num_parts
+    remainder = total_lines % num_parts
+    
+    for i in range(num_parts):
+        part_lines = lines[i * chunk_size : (i+1) * chunk_size]
+        if i == num_parts - 1:
+            part_lines += lines[-remainder:]
+            
+        with open(f'file_part_{i + 1}.txt', 'w') as part_file:
+            part_file.writelines(part_lines)
+        
+        print(f"Split {total_lines} lines into {num_parts) files.")
+        
+    if __name__ == "__main__":
+        split_file('file1.txt', 10)
