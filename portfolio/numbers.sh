9814c0dff208885866936f822a52717c56ee8ea9
Lincoln Quick <lincolnmq@gmail.com>
2025-03-30 15:11:57 -0400

    Modify numbers.sh to generate 1 million numbers each for two files, file1.txt and file2.txt

--- portfolio/numbers.sh
+++ portfolio/numbers.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# This script generates 1,000,000 random numbers and stores them in file1.txt, 
-# and outputs the total time in seconds to run the script.
+# This script generates two files, each with 1 million random numbers: 
+# file1.txt and file2.txt.
 
 # First display system time
 echo "Start time: $(date)"
-
 SECONDS=0
 
-rm -f  file1.txt
+rm -f  file1.txt file2.txt
 
 for ((i=1; i<=1000000; i++))
 do
    echo $RANDOM >> file1.txt
+   echo $RANDOM >> file2.txt
 done
 
 # Display elapsed seconds
